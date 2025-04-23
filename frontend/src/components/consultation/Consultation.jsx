eb6880a962c7b9b4ddd37a84c2b0135db58edd5c
fakharabbasbhatti <fakharabbasbhatti333@gmail.com>
2025-04-22 23:07:20 -0700

    Consultation  page

---
+++ frontend/src/components/consultation/Consultation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Consultation = () => {
+  return (
+    <div>
+      <h1>gdgygd</h1>
+    </div>
+  )
+}
+
+export default Consultation;
---
+++ frontend/src/pages/Consultation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ConsultationDetail from '../components/consultation/Consultation'
+
+
+const Consultation = () => {
+  return (
+    <div>
+      <ConsultationDetail />
+    </div>
+  )
+}
+
+export default Consultation

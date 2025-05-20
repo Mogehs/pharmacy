01c1e6414653eea0f6ccf9e51a0514550ff11c08
Mogehs <mugheesurrehman06@gmail.com>
2025-05-20 12:16:44 +0500

    backend stripe session updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -210,7 +210,7 @@ export const loginUser = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .status(200)
